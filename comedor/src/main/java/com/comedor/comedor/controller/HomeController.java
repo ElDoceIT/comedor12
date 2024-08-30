48a60993ecbd7d2f38d9b3934eff5c44b2ab1585
ElDoceIT <it@eldoce.com.ar>
2024-08-30 12:26:01 -0300

    estuvimos jugando con Leo

--- comedor/src/main/java/com/comedor/comedor/controller/HomeController.java
+++ comedor/src/main/java/com/comedor/comedor/controller/HomeController.java
@@ -1,14 +1,8 @@
 package com.comedor.comedor.controller;
 
 
-import com.comedor.comedor.model.Comida;
-import com.comedor.comedor.repository.ComidaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -17,7 +11,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String Home(){
-        return "Home";
+        return "home";
     }
 
     @GetMapping("/fer")
