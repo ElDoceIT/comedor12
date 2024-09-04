087abe13b1c113d3c01f1303c2434c921d841202
Waldo <zero.waldo@gmail.com>
2024-09-04 14:46:38 -0300

    solucionado el tema de js

--- comedor/src/main/resources/static/js/script.js
+++ comedor/src/main/resources/static/js/script.js
@@ -1,16 +1,19 @@
 const listaMenus = document.getElementById('listaMenus');
 const fechaInput = document.getElementById('fecha');
 const menuInput = document.getElementById('menu');
+const agregarButton = document.getElementById('button-addon2');
 
+// Función para agregar un nuevo ítem a la lista
 function agregarItem() {
   const fecha = fechaInput.value;
   const menu = menuInput.value;
 
+  // Verificamos si se ingresaron tanto la fecha como el menú
   if (fecha && menu) {
     const newItem = document.createElement('li');
     newItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'px-3');
 
-    // Convertir la fecha al formato dd/mm/aaaa en UTC
+    // Convertimos la fecha al formato dd/mm/aaaa en UTC
     const fechaFormateada = new Date(fecha).toLocaleDateString('es-ES', { timeZone: 'UTC' });
 
     const itemContent = document.createElement('div');
@@ -18,7 +21,7 @@ function agregarItem() {
 
     newItem.appendChild(itemContent);
 
-    // boton para eliminar
+    // Botón para eliminar el ítem
     const btnEliminar = document.createElement('button');
     btnEliminar.classList.add('btn', 'btn-danger', 'btn-sm', 'rounded-circle');
     btnEliminar.textContent = 'X';
@@ -28,11 +31,17 @@ function agregarItem() {
     newItem.appendChild(btnEliminar);
 
     listaMenus.appendChild(newItem);
-    menuInput.value = '';
+    menuInput.value = ''; // Limpiamos el campo de entrada del menú
+  } else {
+    // Mostrar un mensaje de error si no se ingresaron todos los datos
+    alert('Por favor, ingresa tanto la fecha como el menú.');
   }
 }
 
-// Evita el envío del formulario
+// Agregamos un evento de clic al botón "Agregar"
+agregarButton.addEventListener('click', agregarItem);
+
+// Evitamos que el formulario se envíe de forma predeterminada
 const formulario = document.querySelector('form');
 formulario.addEventListener('submit', (event) => {
   event.preventDefault();
