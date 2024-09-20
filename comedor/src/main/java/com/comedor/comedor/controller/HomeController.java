7dd66e839054971221de64f50ee29dd83ecf56c7
leomencho <leobollini@gmail.com>
2024-09-20 00:07:57 -0300

    se cambio pagina de login y logout

--- comedor/src/main/java/com/comedor/comedor/controller/HomeController.java
+++ comedor/src/main/java/com/comedor/comedor/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.comedor.comedor.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -23,6 +25,21 @@ public class HomeController {
     public String HomeWaldo(){
         return "waldo";
     }
+
+    @GetMapping("/login" )
+    public String mostrarLogin() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        SecurityContextLogoutHandler logoutHandler =
+                new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, null, null);
+        return "redirect:/login";
+    }
+
+
 
 
 
